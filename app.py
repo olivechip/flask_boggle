8d5606fa1966890493e17fcc920309ecca2dd3f9
Oliver <oliver@MSI>
2023-04-11 20:40:54 -0700

    completed

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from boggle import Boggle
-from flask import Flask, session, render_template, request
+from flask import Flask, session, render_template, request, flash, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -10,12 +10,31 @@ boggle_game = Boggle()
 
 @app.route('/')
 def show_board():
+    """generates a new game board, displays current hi-score"""
     game = boggle_game.make_board()
     session['board'] = game
-    return render_template('board.html', game = game)
+    return render_template('board.html', game = session['board'], high_score = session['high_score'])
 
 @app.route('/guess')
 def try_guess():
+    """returns a valid or invalid response based on input, increments score if valid"""
     guess = request.args['guess']
-    print(request.args)
-    return render_template('board.html', game = session['board'])
+    response = boggle_game.check_valid_word(session['board'], guess)
+    return jsonify({'result': response})
+
+@app.route('/score', methods=['POST'])
+def save_score():
+    """at game end, if score is highest, saves hi-score"""
+    score_data = request.get_json()
+    score = score_data['totalScore']
+    if score > session['high_score']:
+        session['high_score'] = score
+    return str(session['high_score'])
+    
+@app.route('/reset', methods=['POST'])
+def reset_score():
+    """resets the hi-score"""
+    score_data = request.get_json()
+    score = score_data['totalScore']
+    session['high_score'] = score
+    return str(session['high_score'])
--- static/app.js
+++ static/app.js
@@ -1,13 +1,75 @@
-const $guessButton = document.querySelector('#guessButton')
-const $guessValue = document.querySelector('#guessValue')
+const guessButton = document.querySelector('#guessButton');
+const guessInput = document.querySelector('#guessInput');
+const response = document.querySelector('#response');
+const score = document.querySelector('#score');
+const time = document.querySelector('#time');
+const highScore = document.querySelector('#high_score');
+const resetbutton = document.querySelector('#reset');
 
-$guessButton.addEventListener('click', function(e){
+guessedWords = [];
+totalScore = 0;
+
+guessButton.addEventListener('click', function(e){
     e.preventDefault();
-    makeRequest();
+    if (!guessInput.value){
+    } else {
+        makeRequest(guessInput.value);
+    }
 })
 
-async function makeRequest(){
-    guess = { params: {'guess': $guessValue } }
-    const res = await axios.get('https://127.0.0.1:5000/guess/', guess);
-    console.log(res);
+async function makeRequest(value){
+    guess = { params: {'guess': value } }
+    const res = await axios.get('http://127.0.0.1:5000/guess', guess);
+    result = res.data.result;
+        if (result === 'ok'){
+            if (!guessedWords.includes(value)){
+                guessedWords.push(value);
+                totalScore += value.length;
+                score.innerText = totalScore;
+                guessInput.value = '';
+                response.innerText = `${value} is a valid word!`;
+            } else {
+                guessInput.value = '';
+                response.innerText = `${value} has already been guessed!`;
+            }
+        } else if (result == 'not-on-board'){
+            guessInput.value = '';
+            response.innerText = `${value} is not on the board!`;
+        } else {
+            guessInput.value = '';
+            response.innerText = `${value} is not a valid word!`;
+        }
 }
+
+function startTimer(seconds){
+    let counter = seconds
+    const interval = setInterval(() =>{
+        time.innerText = counter + 's';
+        counter--;
+
+        if (counter < 0){
+            clearInterval(interval);
+            guessButton.disabled = true;
+            guessInput.disabled = true;
+            sendScore();
+        }
+    }, 1000)
+}
+
+async function sendScore(){
+    const res = await axios.post('http://127.0.0.1:5000/score', {'totalScore': totalScore});
+    highScore.innerText = res.data;
+}
+
+
+resetbutton.addEventListener('click', function(e){
+    e.preventDefault();
+    resetScore();
+})
+
+async function resetScore(){
+    const res = await axios.post('http://127.0.0.1:5000/reset', {'totalScore': 0});
+    highScore.innerText = res.data;
+}
+
+window.addEventListener('load', startTimer(30));
